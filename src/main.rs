75c1b1dc8a31dd96be3563ad6a8e089150e81ea0
pwh-pwh <plmoknpwh@163.com>
2024-09-18 21:56:39 +0800

    use send with new account

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@ use std::thread;
 use std::time::Duration;
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::native_token::LAMPORTS_PER_SOL;
-use solana_sdk::{commitment_config::CommitmentConfig, pubkey::Pubkey};
+use solana_sdk::{commitment_config::CommitmentConfig, pubkey::Pubkey, system_instruction};
 
 use clap::{Parser, ValueEnum};
+use solana_sdk::signature::Keypair;
+use solana_sdk::signer::Signer;
+use solana_sdk::system_transaction;
+use solana_sdk::transaction::Transaction;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -30,7 +34,28 @@ enum Mode {
     Testnet,
 }
 
-fn get_airdrop(client:&RpcClient,pk:&Pubkey) {
+fn create_account()->Keypair {
+    Keypair::new()
+}
+
+fn send_with_new_account(client:&RpcClient,pk: &Pubkey) {
+    let new_account = Keypair::new();
+    match get_airdrop(client, &new_account.pubkey()) {
+        Ok(_) => {
+            let hash = client.get_latest_blockhash().unwrap();
+            let tr = system_instruction::transfer(&new_account.pubkey(),pk,LAMPORTS_PER_SOL /10 * 49);
+            let mut transaction = Transaction::new_with_payer(&[tr], Some(&new_account.pubkey()));
+            transaction.sign(&[&new_account], hash);
+            let signature = client.send_and_confirm_transaction(&transaction).unwrap();
+            println!("{}", signature);
+        }
+        Err(e) => {
+            println!("Err: {:?}", e);
+        }
+    }
+}
+
+fn get_airdrop(client:&RpcClient,pk:&Pubkey)->Result<(), String> {
     let airdrop_amount = LAMPORTS_PER_SOL * 5;
     let signature = client.request_airdrop(&pk, airdrop_amount);
     match signature {
@@ -43,9 +68,11 @@ fn get_airdrop(client:&RpcClient,pk:&Pubkey) {
                 }
             }
             println!("airdrop 5 sol to {} success", &pk.to_string());
+            Ok(())
         }
         Err(err) => {
             println!("{:?}", err);
+            Err("airdrop failed".into())
         }
     }
 }
@@ -59,7 +86,7 @@ fn main() {
     let client = RpcClient::new_with_commitment(rpc_url.to_string(), CommitmentConfig::confirmed());
     let pk = Pubkey::from_str(&args.address).unwrap();
     loop {
-        get_airdrop(&client,&pk);
-        thread::sleep(Duration::from_secs(1800));
+        send_with_new_account(&client,&pk);
+        thread::sleep(Duration::from_secs(1));
     }
 }
